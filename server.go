ef3b7d4b5ab18ed25f35fb625381cc0a2f2c30b8
U-haru <ugaharutaka@gmail.com>
2022-04-08 15:46:58 +0900

    エラーページ追加

--- server.go
+++ server.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strconv"
 )
 
-// Websocketに流れてきたパケットをsocks5として解釈するサーバー
 type Server struct {
 	Path   string
 	Host   string
@@ -28,31 +28,64 @@ func (s *Server) ListenAndServe() (err error) {
 }
 
 func (s *Server) Serve(li net.Listener) (err error) {
-	s.HandleFunc(s.Path, func(w http.ResponseWriter, r *http.Request) {
-		hj, ok := w.(http.Hijacker)
-		if !ok {
-			log.Println("Error")
-			return
-		}
-		sv, err := net.Dial("tcp", s.Target)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		cl, _, err := hj.Hijack()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		go func() {
-			io.Copy(cl, sv)
-			sv.Close()
-		}()
-		go func() {
-			io.Copy(sv, cl)
-			cl.Close()
-		}()
-	})
+	s.HandleFunc(s.Path, s.handle)
+	s.HandleFunc("/", errorp)
 
 	return http.Serve(li, s)
+}
+
+const message = `<!DOCTYPE html>
+<html>
+<head>
+<title>Error</title>
+<style>
+body { width: 96%; margin: 0 auto; }
+</style>
+</head>
+<body>
+<center><h1>500 Internal Server Error</h1>
+<hr>pipe</center>
+</body>
+</html>`
+
+func errorp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Length", strconv.Itoa(len(message)))
+	w.WriteHeader(500)
+	w.Write([]byte(message))
+}
+
+func (s *Server) handle(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "PUT", "PUSH":
+		{
+			hj, ok := w.(http.Hijacker)
+			if !ok {
+				log.Println("Error")
+				return
+			}
+			sv, err := net.Dial("tcp", s.Target)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			cl, _, err := hj.Hijack()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			go func() {
+				io.Copy(cl, sv)
+				sv.Close()
+			}()
+			go func() {
+				io.Copy(sv, cl)
+				cl.Close()
+			}()
+		}
+	default:
+		{
+			errorp(w, r)
+		}
+	}
 }
